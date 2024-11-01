caaf6144e78282e1b8695bd3de52c2ebd2391c47
Carlos <Carlos@DESKTOP-M95EH9U>
2024-10-31 17:16:23 -0700

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AutomationPractice
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
