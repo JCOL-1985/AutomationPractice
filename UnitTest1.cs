49ffefaa866f44ca2bed437a1d9c91c953183bf0
JCOL-1985 <jcol_19@outlook.com>
2024-11-04 16:58:08 -0700

    TC created

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,21 +1,56 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Security.Cryptography.X509Certificates;
+
 namespace AutomationPractice
 {
     public class Tests
     {
+        private IWebDriver _driver;
+        private HomePage _homePage;
+
         [SetUp]
         public void Setup()
         {
+          _driver = new ChromeDriver ();_driver.Navigate().GoToUrl("https://www.walmart.com.mx/inicio");
+          _driver.Manage().Window.Maximize();
+          _homePage = new HomePage(_driver);
         }
 
         [TearDown]
         public void TearDown()
         {
+            _driver.Quit();
+            _driver.Dispose();
         }
 
-        [Test]
+        [Test, Order(1)]
         public void Test1()
         {
-            Assert.Pass();
+            _homePage.Search("");
+            Assert.That(_driver.Title, Does.Contain("Haz tus compras por internet | Walmart tienda en línea"));
+        }
+        [Test,Order(2)]
+        public void Smarphone()
+        {
+            _homePage.Search("Smarphone");
+            _homePage.Searching();
+            _homePage.SearchSmarphone();
+            Assert.That(_homePage.validateResultSmarphone, Is.EqualTo(true));
+        }
+        [Test,Order(3)]
+        public void SearchFridge()
+        {
+            _homePage.Search("refrigerador");
+            _homePage.Searching();
+            _homePage.SearchFridge();
+            Assert.That(_driver.Title, Does.Contain("walmart.com.mx/cart"));
+
+        }
+        public void DeleteShoppingCard()
+        {
+            _homePage.Search("");
+            Assert.That(_homePage.validateDeleteResult, Is.EqualTo(true));
         }
     }
 }
